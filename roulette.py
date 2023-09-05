2c87db98280a5806ba61e2e736b059b1acf4f435
PXDX <51523749+rankspitter@users.noreply.github.com>
2023-09-06 02:04:10 +0700

    not completed

--- roulette.py
+++ roulette.py
@@ -1,14 +1,29 @@
+import random
+
+
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+
 class player:
     def __init__(self, money):
-        self.money = money
-
-
+        self.player = money
+        self.deler = 0
 
 
 class roulette:
-    def __init__(self, ball) :
-        self.ball = ball
+    def __init__(self):
+        self.ball = 1
+        self.wheel = [random.randint(0, 36) for i in range(37)  ]
+        self.last = None
 
+
+    def spin_the_wheel(self):
+        pass
 
 
 
@@ -16,34 +31,20 @@ class roulette:
 
 class betting_table:
     def __init__(self):
-        pass
-
-    def roulette_board(self):
-        number = []
-        for i in range (1,37):
-            number.append(i)
-        return print(number)
-
-
-
-
-
-
-
-
-class game:
-    board = betting_table()
-    board.roulette_board()
-
-
-
+        self.table = [x for x in range(37)]
 
 
 
 
 if __name__ == "__main__":
-    start = game()
 
+    money = int(input("How much money do you have? : "))
+    play = player(money)
 
+    r = roulette()
+    print('-'*100)
+    print(r.wheel)
+    print('-'*100)
 
-
+    table = betting_table()
+    print(table.table)
